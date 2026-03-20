fc66365335e89fdd6f81c928cd8d1c5d34580a83
andrey <andrey@xaracal.com>
2026-03-20 17:19:22 +0200

    Use typescript to specify express app has been used.

--- miniwall-app/src/main.ts
+++ miniwall-app/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { type NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<string>('PORT', '3000'));
 }
 bootstrap();
